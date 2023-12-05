25077f9b9e29812411c3fea00eaf488d5e278962
Kobayashi, L Mitiko <105651010+Mitiko20@users.noreply.github.com>
2023-12-04 23:03:02 -0300

    Add Project BackEnd in Vercel

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,10 +1,19 @@
-const mongoose = require('mongoose')
+require('dotenv').config({ path: '.env.local' });  // Carrega as variáveis de ambiente do arquivo .env.local
+
+const mongoose = require('mongoose');
 
 async function main() {
-  await mongoose.connect('mongodb+srv://root:root@cluster.99chqhn.mongodb.net/?retryWrites=true&w=majority')
-  console.log('Conectou com Mongoose!')
+  const MONGODB_URI = process.env.MONGODB_URI;  // Obtém o valor de MONGODB_URI da variável de ambiente
+
+  if (!MONGODB_URI) {
+    console.error('A variável de ambiente MONGODB_URI não está configurada.');
+    return;
+  }
+
+  await mongoose.connect(MONGODB_URI);
+  console.log('Conectou com Mongoose!');
 }
 
-main().catch((err) => console.log(err))
+main().catch((err) => console.log(err));
 
-module.exports = mongoose
+module.exports = mongoose;
