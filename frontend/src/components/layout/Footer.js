32bcffb896bde8d5079532d5302de84f5fe3847d
Kobayashi, L Mitiko <105651010+Mitiko20@users.noreply.github.com>
2023-12-06 01:01:41 -0300

    Teste vercel com front

---
+++ frontend/src/components/layout/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from "./Footer.module.css";
+
+function Footer() {
+  return <footer className={styles.footer}>
+    <p><b>Seja adotado - DW2A4</b> &copy; 2023</p>
+  </footer>
+}
+
+export default Footer
